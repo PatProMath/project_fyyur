eb570c0980e6b8cdbde7d86a7e511af638b91192
douglasbergman <68873911+douglasbergman@users.noreply.github.com>
2021-12-03 09:12:58 -0700

    updated starter code into root from starter_code folder
    updated starter code into root from starter_code folder

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
@@ -9,3 +10,18 @@ DEBUG = True
 # Connect to the database
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://patty:pos02@localhost:5432/fyyur_db'
+=======
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+>>>>>>> 3891aaf (updated starter code into root from starter_code folder)
